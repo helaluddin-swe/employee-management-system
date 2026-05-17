31ee6e6afea5ce1b86fd0e05d512970f4c3d517e
Helal Uddin -SWE <seoguro1999@gmail.com>
2026-05-17 23:14:18 +0600

    add sidebar css styling

--- client/src/componenets/Sidebar.jsx
+++ client/src/componenets/Sidebar.jsx
@@ -16,16 +16,18 @@ const Sidebar = () => {
     {name:"Settings",path:"/employee/settings", icon:Settings2},
   ]
   return (
-    <div>
-      {isMobileMenu && (<button onClick={()=>setIsMobileMenu(!isMobileMenu)}> <MenuIcon size={22}/></button>)}
+    <div className="fixed top-0 left-0 overflow-y-auto h-screen w-20 md:w-64 scrollbar-thin scrollbar-thumb-zinc-400 max-md:-translate-x-full max-md:open:translate-0 transition-all ">
+      <h2 className="p-6 border-b border-gray-200">EMS systems</h2>
+      <div className="flex flex-col gap-4 pt-6"> 
      {sidebarContent.map((item,i)=>(
       <Link key={i} to={item.path} > 
-       <div className="flex gap-2 items-center justify-center "> 
-        <item.icon/>
+       <div className=" hover:bg-gray-100 hover:translate-x-2 transition-all duration-300 active:bg-blue-600 active:text-white active:font-semibold flex gap-2 py-2 px-2 rounded-md "> 
+        <item.icon className=""/>
         <p className="hidden md:block font-semibold">{item.name}</p>
        </div>
       </Link>
      ))}
+     </div>
 
       {/* desktop */}
       
